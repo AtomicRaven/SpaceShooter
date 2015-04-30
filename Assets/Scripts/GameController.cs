2cc19bc054cf3625e3db20a52bc2db751b843fd4
Rob Clarkson <rob.clarkson@gmail.com>
2015-04-30 15:12:00 -0400

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+
+	private bool gameOver;
+	private bool reStart;
+	private int score;
+
+	void Start()
+	{
+		score = 0;
+		gameOver = false;
+		reStart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+
+		UpdateScore();
+		StartCoroutine(SpawnWaves());
+	}
+
+	void Update(){
+
+		if(reStart)
+		{
+			if(Input.GetKeyDown(KeyCode.R))
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+
+		while(true){
+
+			for(int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x, spawnValues.x),spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds(spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+
+			if(gameOver)
+			{
+				restartText.text = "Press 'R' to Restart";
+				reStart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+
+	}
+
+}
